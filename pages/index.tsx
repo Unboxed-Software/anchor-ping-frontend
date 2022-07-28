d2546a017b5a8612ccc6ea1508e2a37f78d6de00
john <johnzhengyuliu@gmail.com>
2022-07-28 00:50:31 -0500

    solution code

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,30 +1,25 @@
-import { NextPage } from 'next'
-import styles from '../styles/Home.module.css'
-import WalletContextProvider from '../components/WalletContextProvider'
-import { AppBar } from '../components/AppBar'
-import { BalanceDisplay } from '../components/BalanceDisplay'
-import { PingButton } from '../components/PingButton'
-import Head from 'next/head'
+import { NextPage } from "next"
+import styles from "../styles/Home.module.css"
+import WalletContextProvider from "../components/WalletContextProvider"
+import { AppBar } from "../components/AppBar"
+import { PingButton } from "../components/PingButton"
+import Head from "next/head"
 
 const Home: NextPage = (props) => {
-
   return (
     <div className={styles.App}>
       <Head>
         <title>Wallet-Adapter Example</title>
-        <meta
-          name="description"
-          content="Wallet-Adapter Example"
-        />
+        <meta name="description" content="Wallet-Adapter Example" />
       </Head>
       <WalletContextProvider>
         <AppBar />
         <div className={styles.AppBody}>
           <PingButton />
         </div>
-      </WalletContextProvider >
+      </WalletContextProvider>
     </div>
-  );
+  )
 }
 
-export default Home;
+export default Home
