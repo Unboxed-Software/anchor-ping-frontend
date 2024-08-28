48e4445d6a0cb61a29b9291c0712e8be1a0d1cfa
aryan <agarwalaryan139@gmail.com>
2024-08-28 06:24:37 +0530

    bump anchor to 0.30.1

--- components/Increment.tsx
+++ components/Increment.tsx
@@ -3,12 +3,11 @@ import {
   useWallet,
   useAnchorWallet,
 } from "@solana/wallet-adapter-react"
-import * as anchor from "@project-serum/anchor"
+import * as anchor from "@coral-xyz/anchor"
 import { FC, useCallback, useEffect, useState } from "react"
 import idl from "../idl.json"
 import { Button, HStack, VStack, Text } from "@chakra-ui/react"
-
-const PROGRAM_ID = `9pbyP2VX8Rc7v4nR5n5Kf5azmnw5hHfkJcZKPHXW98mf`
+import { AnchorCounter } from "../types/AnchorCounter"
 
 export interface Props {
   counter
@@ -17,7 +16,7 @@ export interface Props {
 
 export const Increment: FC<Props> = ({ counter, setTransactionUrl }) => {
   const [count, setCount] = useState(0)
-  const [program, setProgram] = useState<anchor.Program>()
+  const [program, setProgram] = useState<anchor.Program<AnchorCounter>>()
   const { connection } = useConnection()
   const wallet = useAnchorWallet()
 
@@ -31,7 +30,7 @@ export const Increment: FC<Props> = ({ counter, setTransactionUrl }) => {
       anchor.setProvider(provider)
     }
 
-    const program = new anchor.Program(idl as anchor.Idl, PROGRAM_ID)
+    const program = new anchor.Program(idl as AnchorCounter) as anchor.Program<AnchorCounter>
     setProgram(program)
     refreshCount(program)
   }, [])
